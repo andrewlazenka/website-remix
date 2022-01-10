9e9f9414820faa14c78d6e7a7ae94f4211383ffb
Andrew Lazenka <andrewlazenka@gmail.com>
2022-01-09 23:18:46 -0500

    Fix wonky theme toggle

--- app/components/ModeToggle.tsx
+++ app/components/ModeToggle.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import clsx from 'classnames'
 
 import SunIcon from '~/components/svg/icons/sun'
 import MoonIcon from '~/components/svg/icons/moon'
 
 const baseIconStyle =
   'absolute cursor-pointer transition duration-300 ease-in-out hover:text-indigo-300 text-gray-50'
-const hiddenStyle = 'opacity-0 invisible'
-const shownStyle = 'opacity-100 visible'
 
 type Themes = 'dark' | 'light'
 
 function ModeToggle() {
-  // fix for Gatsby SSR when window is not yet defined
+  // fix for SSR when window is not yet defined
   let initialTheme = null
   if (typeof window !== `undefined`) {
     initialTheme = window.__theme
@@ -20,42 +17,26 @@ function ModeToggle() {
 
   const [theme, setTheme] = React.useState(initialTheme)
 
-  React.useEffect(() => {
-    window.__onThemeChange = () => {
-      setTheme(window.__theme)
-    }
-    setTheme(window.__theme)
-  }, [])
-
-  function changeTheme(newMode: Themes) {
-    setTheme(newMode)
-    window.__setPreferredTheme(newMode)
+  function changeTheme() {
+    const updatedTheme: Themes = isDarkTheme ? 'light' : 'dark'
+    setTheme(updatedTheme)
+    window.__setPreferredTheme(updatedTheme)
   }
 
   const isDarkTheme = theme === 'dark'
 
-  // fix for Gatsby SSR when window is not yet defined
+  // fix for SSR when window is not yet defined
   if (theme === null) {
     return <div className="h-6 w-6" />
   }
 
+  const ToggleIcon = isDarkTheme ? MoonIcon : SunIcon
+
   return (
     <span className="relative w-6 h-6 leading-[0px]">
-      <SunIcon
-        className={clsx(
-          baseIconStyle,
-          { [shownStyle]: !isDarkTheme },
-          { [hiddenStyle]: isDarkTheme }
-        )}
-        onClick={() => changeTheme('dark')}
-      />
-      <MoonIcon
-        className={clsx(
-          baseIconStyle,
-          { [hiddenStyle]: !isDarkTheme },
-          { [shownStyle]: isDarkTheme }
-        )}
-        onClick={() => changeTheme('light')}
+      <ToggleIcon
+        className={baseIconStyle}
+        onClick={changeTheme}
       />
     </span>
   )
