ff8874f4803f938fa2b3582a6c90d9a1cb5688b3
Andrew Lazenka <andrewlazenka@gmail.com>
2022-01-11 19:40:41 -0500

    start extending tiptak editor config

--- app/routes/editor.tsx
+++ app/routes/editor.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { format } from 'date-fns'
+import { marked } from 'marked'
 import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import Heading from '@tiptap/extension-heading'
 
 const EditorPage = () => {
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      Document,
+      Paragraph,
+      Text,
+      Heading.configure({
+        levels: [1, 2, 3, 4, 5, 6],
+      }),
     ],
-    content: '<p>Hello World!</p>',
     editorProps: {
       attributes: {
-        class: "prose lg:prose-xl"
-      }
-    }
+        class: 'prose lg:prose-xl',
+      },
+    },
   })
 
   function saveContent() {
@@ -20,14 +29,36 @@ const EditorPage = () => {
       return
     }
 
-    console.log(editor.getHTML())
+    const md = marked.parse(editor.getText())
+    console.log('🚀 ~ file: editor.tsx ~ line 24 ~ saveContent ~ md', md)
   }
+
+  const initialDate = format(new Date(), 'yyyy-MM-dd')
 
   return (
     <main>
       <h1>Editor Page</h1>
-      <EditorContent editor={editor} />
-      <button onClick={saveContent}>Save Content</button>
+      <div className="flex flex-col max-w-2xl m-auto">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          required
+          className="my-4"
+        />
+        <input
+          type="date"
+          name="created_at"
+          className="my-4"
+          defaultValue={initialDate}
+        />
+        <section className="bg-gray-50 p-8 rounded-xl drop-shadow-lg my-4">
+          <EditorContent editor={editor} />
+        </section>
+        <button onClick={saveContent} className="my-4">
+          Save Content
+        </button>
+      </div>
     </main>
   )
 }
