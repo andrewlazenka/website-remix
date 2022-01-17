3a465a33795de69cefd72803c7e11bab280b271e
Andrew Lazenka <andrewlazenka@gmail.com>
2022-01-17 15:05:25 -0500

    Restrict hero height

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -4,7 +4,7 @@ import clsx from 'classnames'
 const HeroBanner = ({ fullHeight = false, ...props }) => (
   <div
     className={clsx(
-      'h-screen relative bg-cover transition-colors duration-300 ease-in-out bg-blue-500 dark:bg-gray-900 bg-pattern-triangles',
+      'h-screen relative max-h-[700px] bg-cover transition-colors duration-300 ease-in-out bg-blue-500 dark:bg-gray-900 bg-pattern-triangles',
     )}
     {...props}
   />
