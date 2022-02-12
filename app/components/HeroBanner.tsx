37bc2424271ec8d490a1f15ca928ff868e90be39
Andrew Lazenka <andrewlazenka@gmail.com>
2022-02-12 13:32:56 -0500

    tweak styles

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -4,8 +4,8 @@ import clsx from 'classnames'
 const HeroBanner = ({ slim = false, ...props }) => (
   <div
     className={clsx(
-      'relative mx-auto w-10/12 mb-24 bg-blue-500 bg-pattern-wavey rounded-md transition-colors duration-300 ease-in-out dark:bg-sky-50 dark:text-gray-900 text-gray-50',
-      { 'pb-24 pt-32 px-12': slim === false }
+      'relative mx-auto w-10/12 px-12 mb-24 bg-pattern-wavey rounded-md transition-colors duration-300 ease-in-out dark:bg-white bg-blue-500  dark:text-gray-900 text-gray-50',
+      { 'pb-24 pt-32': slim === false }
     )}
     {...props}
   />
--- app/routes/notebook/$slug/edit.tsx
+++ app/routes/notebook/$slug/edit.tsx
@@ -103,7 +103,7 @@ const NotebookEdit = () => {
             className="my-4"
             defaultValue={initialDate}
           />
-          <section className="my-4 rounded-xl bg-gray-50 p-8 drop-shadow-lg">
+          <section className="my-4 rounded-xl bg-white p-8 drop-shadow-lg">
             <EditorContent editor={editor} />
           </section>
           <input type="hidden" name="content" value={editor?.getHTML()} />
