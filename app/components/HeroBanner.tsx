ee666070386bb6c8baac259f04cde0ab87cf2fb6
Andrew Lazenka <andrewlazenka@gmail.com>
2022-02-12 14:23:38 -0500

    fade in hero banners

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import clsx from 'classnames'
+import { animated, useSpring } from 'react-spring'
 
-const HeroBanner = ({ slim = false, ...props }) => (
-  <div
-    className={clsx(
-      'relative mx-auto w-10/12 px-12 mb-24 bg-pattern-wavey rounded-md transition-colors duration-300 ease-in-out dark:bg-white bg-blue-500  dark:text-gray-900 text-gray-50',
-      { 'pb-24 pt-32': slim === false }
-    )}
-    {...props}
-  />
-)
+const HeroBanner = ({ slim = false, ...props }) => {
+  const fade = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: {
+      duration: 750,
+    },
+  })
+  return (
+    <animated.div
+      style={fade}
+      className={clsx(
+        'relative mx-auto mb-24 w-10/12 rounded-md bg-blue-500 bg-pattern-wavey px-12 text-gray-50 transition-colors duration-300 ease-in-out  dark:bg-white dark:text-gray-900',
+        { 'pb-24 pt-32': slim === false }
+      )}
+      {...props}
+    />
+  )
+}
 
 export default HeroBanner
