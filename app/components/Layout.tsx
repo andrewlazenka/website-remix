7582aaaeb09f877260d0a27cb4f961835862bab5
Andrew Lazenka <andrewlazenka@gmail.com>
2023-02-16 21:40:48 -0500

    Add animation into main Layout component

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { useSpring, animated } from 'react-spring'
 
-const Layout = (props: any) => (
-  <main
-    style={{ minHeight: 'calc(100vh - 292px)' }}
-    className="mx-auto w-3/4 max-w-3xl p-6"
-    {...props}
-  />
-)
+const Layout = ({ noFade = false, ...props }: any) => {
+	const fade = useSpring({
+		to: { opacity: 1 },
+		from: { opacity: 0 },
+		config: {
+		  duration: 750,
+		},
+	})
+
+	let style = { minHeight: 'calc(100vh - 292px)' }
+	if (!noFade) {
+		style = { ...style, ...fade }
+	}
+
+	return (
+	  <animated.main
+		style={style}
+		className="mx-auto w-3/4 max-w-3xl p-6"
+		{...props}
+	  />
+	)
+}
 
 export default Layout
